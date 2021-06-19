017534cfaa85bf4ffc4d8dab1a6ac8e0710eb06f
Eyal Porat <73476317+ViciiSystem@users.noreply.github.com>
2021-06-19 22:14:13 +0300

    Update Kalman.h

--- src/Kalman.h
+++ src/Kalman.h
@@ -1,1 +1,20 @@
+//Wrote by Eyal Porat from Vicii Air Systems in 2021
+//an implementation of the kalman filter
+//Vicii Air Systems youtube channel https://www.youtube.com/channel/UCOSavZxZ2kfVIxUxBTmdQTg
+//Copyleft licenced
 
+#pragma once
+
+class KalmanC
+{
+  public:
+    KalmanC(float _R);
+    const float R;
+    const float H;
+    float Q;
+    float P;
+    float U_hat;
+    float K;
+    float KALMAN(float U);
+  
+};
