545590f6a123e0a46d701097f9ca8fe01203dbe8
Eyal Porat <73476317+ViciiSystem@users.noreply.github.com>
2021-06-19 22:04:14 +0300

    Update and rename d_Kalman.h to Kalman.h

---
+++ Kalman.h
@@ -0,0 +1,19 @@
+//Wrote by Eyal Porat from Vicii Air Systems in 2021
+//Vicii Air Systems youtube channel https://www.youtube.com/channel/UCOSavZxZ2kfVIxUxBTmdQTg
+//Copyleft licenced
+
+#pragma once
+
+class KalmanC
+{
+  public:
+    KalmanC(float _R);
+    const float R;
+    const float H;
+    float Q;
+    float P;
+    float U_hat;
+    float K;
+    float KALMAN(float U);
+  
+};
