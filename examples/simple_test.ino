a435ed87728e36c2c9c60ca6150bfb7b84aef087
Eyal Porat <73476317+ViciiSystem@users.noreply.github.com>
2021-06-19 22:48:53 +0300

    Update simple_test.ino

--- examples/simple_test.ino
+++ examples/simple_test.ino
@@ -1,4 +1,9 @@
 //simple arduino example
+  
+//Wrote by Eyal Porat from Vicii Air Systems in 2021
+//an implementation of the kalman filter
+//Vicii Air Systems youtube channel https://www.youtube.com/channel/UCOSavZxZ2kfVIxUxBTmdQTg
+//Copyleft licenced
 
 #include "Kalman.h"
 
