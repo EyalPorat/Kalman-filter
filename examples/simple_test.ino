fbca3ae1c59e791a7ea32f90031aa375fa48e52d
Eyal Porat <73476317+ViciiSystem@users.noreply.github.com>
2021-06-19 22:59:51 +0300

    Update simple_test.ino

--- examples/simple_test.ino
+++ examples/simple_test.ino
@@ -13,21 +13,23 @@ KalmanC myKalman = KalmanC(1000);
 int noisy_signal[50] = {0, 1, 5, 3, 7, 5, 6, 9, 15, 11, 13, 25, 20, 19, 22, 27, 30, 25, 32, 37, 35, 33, 39, 70, 39, 42, 35, 38, 44, 50, 47, 55, 56, 45, 55, 59, 60, 0, 65, 62, 69, 72, 75, 79, 70, 75, 80, 88, 90, 100};
 int processed_signal[50];
 
-void setup
+void setup()
 {
   //intilize serial comunication
   Serial.begin(9600);
 }
 
-void loop
+void loop()
 {
   for (int i = 0; i < 50; i++)
   {
     //updating myKalman object with the noisy signal
-    proccesed_signal[i] = myKalman.KALMAN(noisy_signal[i]);
+    processed_signal[i] = myKalman.KALMAN(noisy_signal[i]);
     //printing the value to the serial monitor
-    Serial.println(proccesed_signal[i]);
+    Serial.print(noisy_signal[i]);
+    Serial.print("  ");
+    Serial.println(processed_signal[i]);
     //small delay
-    delay(30);
+    delay(200);
   }
 }
